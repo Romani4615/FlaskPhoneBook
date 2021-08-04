52f6bec227252d1f5d4b0bca211521944674064f
romani4615 <romani4615@gmail.com>
2021-08-04 10:30:03 -0400

    updated HwD2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask_wtf import form
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app.forms import PhoneBook
 
 @app.route('/')
@@ -16,5 +16,5 @@ def phonebook():
         phone_number = form.phone_number.data
         address = form.address.data
         print(name, phone_number, address)
-
+        return redirect(url_for('phonebook'))
     return render_template('PhoneBook.html', title='Your Friendly Neighborhood Phone Book', form=form)
